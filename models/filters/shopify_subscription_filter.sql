6f4878912967f570570d0fc0c8416df46f662724
Andres Recalde <andres.recalde@lacolombe.net>
2016-11-18 14:03:25 -0500

    Changed filter to use source table

--- models/filters/shopify_subscription_filter.sql
+++ models/filters/shopify_subscription_filter.sql
@@ -1,3 +1,3 @@
 select id as order_item_id, order_id, customer_id
-from {{ref('shopify_base_order_items')}}
-where oi.subscription_type = 'Subscription'
+from {{ref('shopify_source_order_items')}}
+where title not like '%Gift Subscription%' and (title like '%Subscription%' or title like '%Auto renew%')
