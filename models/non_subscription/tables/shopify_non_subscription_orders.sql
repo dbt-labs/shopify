6d5e549f16fa423942fe23837f8691545098403e
Andres Recalde <andres.recalde@lacolombe.net>
2016-11-21 12:48:57 -0500

    Added order calculated columns

--- models/non_subscription/tables/shopify_non_subscription_orders.sql
+++ models/non_subscription/tables/shopify_non_subscription_orders.sql
@@ -33,7 +33,13 @@ SELECT
        years_active,
        lifetime_revenue,
        items_purchased,
-       annual_revenue
+       annual_revenue,
+
+--Calculated Columns
+       datediff(second, o.customer_created_at, o.created_at) as time_since_customer_creation,
+       datediff(second, first_order_date, o.created_at) as time_since_first_order,
+       datediff(second, po.created_at, o.created_at) as time_since_previous_order,
+       number_of_orders = o.customer_order_number
 
 FROM {{ref('shopify_base_non_subscription_orders')}} o
 JOIN {{ref('shopify_non_subscription_order_aggregates')}} oa on oa.order_id = o.id
--- models/subscription/tables/shopify_subscription_orders.sql
+++ models/subscription/tables/shopify_subscription_orders.sql
@@ -33,9 +33,21 @@ SELECT
        years_active,
        lifetime_revenue,
        items_purchased,
-       annual_revenue
+       annual_revenue,
+
+--Calculated Columns
+       datediff(second, o.customer_created_at, o.created_at) as time_since_customer_creation,
+       datediff(second, first_order_date, o.created_at) as time_since_first_order,
+       datediff(second, po.created_at, o.created_at) as time_since_previous_order,
+       number_of_orders = o.customer_order_number
 
 FROM {{ref('shopify_base_subscription_orders')}} o
 JOIN {{ref('shopify_subscription_order_aggregates')}} oa on oa.order_id = o.id
 JOIN {{ref('shopify_subscription_customers')}} c on c.id = o.customer_id
 LEFT JOIN {{ref('shopify_base_subscription_orders')}} po on po.customer_id = o.customer_id and po.customer_order_number = (o.customer_order_number - 1)
+
+/*time_since_customer_creation,
+time_since_first_order,
+time_since_previous_order,
+is_most_recent_order,
+*/
