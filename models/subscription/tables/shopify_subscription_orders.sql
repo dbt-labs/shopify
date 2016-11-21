9f14c0d163059657395bdef8e48420d93d861534
Andres Recalde <andres.recalde@lacolombe.net>
2016-11-21 12:35:08 -0500

    Added in previous orders created at

--- models/subscription/tables/shopify_subscription_orders.sql
+++ models/subscription/tables/shopify_subscription_orders.sql
@@ -7,19 +7,20 @@ SELECT
 --Customer Info
        o.email,
 --Order Status
-       financial_status,
-       fulfillment_status,
-       processing_method,
-       gateway,
+       o.financial_status,
+       o.fulfillment_status,
+       o.processing_method,
+       o.gateway,
        
 --Timestamps
        o.created_at,
-       customer_created_at,
+       o.customer_created_at,
        greatest(c.updated_at, oa.updated_at, o.updated_at) as updated_at,
+       po.created_at as previous_order_created_at,
 --Numbers
 	o.subtotal_price,
 	o.total_weight,
-       customer_order_number,
+       o.customer_order_number,
 
 --Order Aggregates
        oa.count_of_items,
@@ -37,3 +38,4 @@ SELECT
 FROM {{ref('shopify_base_subscription_orders')}} o
 JOIN {{ref('shopify_subscription_order_aggregates')}} oa on oa.order_id = o.id
 JOIN {{ref('shopify_subscription_customers')}} c on c.id = o.customer_id
+LEFT JOIN {{ref('shopify_base_subscription_orders')}} po on po.customer_id = o.customer_id and po.customer_order_number = (o.customer_order_number - 1)
