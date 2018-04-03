46660424aa1905e4eefe90987ed91f428e885da2
Andres Recalde <andres.recalde@lacolombe.net>
2018-04-03 13:12:13 -0400

    new logic for subs

--- models/all/base/shopify_base_order_items.sql
+++ models/all/base/shopify_base_order_items.sql
@@ -7,6 +7,7 @@ select
     oi.variant_id,
     o.customer_id,
     o.order_number,
+    o.line_item_number,
 
 --Item Info
     oi.name,
@@ -34,10 +35,17 @@ select
     o.financial_status,
     oi.fulfillment_status,
 
+--Order Item Properties
+subid.property_value as subscription_id,
+sif.property_value as subscription_shipping_frequency,
+siut.property_value as subscription_shipping_frequency_unit,
+cif.property_value as subscription_charge_frequency,
+ciut.property_value as subscription_charge_frequency_unit,
+
   --Calculated Columns
   	case
-    	when oi.title like '%Gift%' and (oi.title like '%Subscription%' or oi.title like '%Auto renew%') then 'Gift Subscription'
-    	when oi.title like '%Subscription%' or oi.title like '%Auto renew%' then 'Subscription'
+    	when cif.property_value is not null then 'Gift Subscription'
+    	when sif.property_value is not null then 'Subscription'
     	else 'Non-Subscription'
   	end as subscription_type
 
@@ -45,3 +53,24 @@ from {{ref('shopify_source_order_items')}} oi
 left join {{ref('shopify_source_products')}} p on p.id = oi.product_id
 left join {{ref('shopify_source_product_variants')}} pv on pv.id = oi.variant_id
 join {{ref('shopify_base_orders')}} o on o.id = oi.order_id
+left join {{ref('shopify_source_order_items_properties')}} subid 
+        on subid.order_id = oi.order_id 
+        and subid.line_item_number = oi.line_item_number 
+        and subid.property_name = 'subscription_id'
+left join {{ref('shopify_source_order_items_properties')}} sif 
+        on sif.order_id = oi.order_id 
+        and sif.line_item_number = oi.line_item_number 
+        and sif.property_name = 'shipping_interval_frequency'
+left join {{ref('shopify_source_order_items_properties')}} siut 
+        on siut.order_id = oi.order_id 
+        and siut.line_item_number = oi.line_item_number 
+        and siut.property_name = 'shipping_interval_unit_type'
+left join {{ref('shopify_source_order_items_properties')}} cif 
+        on cif.order_id = oi.order_id 
+        and cif.line_item_number = oi.line_item_number 
+        and cif.property_name = 'charge_interval_frequency'
+left join {{ref('shopify_source_order_items_properties')}} ciut 
+        on ciut.order_id = oi.order_id 
+        and ciut.line_item_number = oi.line_item_number 
+        and ciut.property_name = 'charge_interval_unit_type'
+
