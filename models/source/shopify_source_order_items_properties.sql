526547763a2306bcaf839ada3ee88bb0b6a94768
Andres Recalde <andres.recalde@lacolombe.net>
2018-09-09 22:38:41 -0400

    fixed stitch split

--- models/source/shopify_source_order_items_properties.sql
+++ models/source/shopify_source_order_items_properties.sql
@@ -2,6 +2,6 @@ select
   _sdc_source_key_id as order_id,
   _sdc_level_0_id as line_item_number,
   name as property_name,
-  value as property_value,
+  nvl(value__string, value__bigint::varchar) as property_value,
   _sdc_received_at as updated_at
 from {{ var('source_schema') }}.{{ var('order_items_properties_table') }}
