23a80927c8db86782340eeb48ba44c96f111b284
Erin Ogilvy <erin.ogilvy@gmail.com>
2019-02-18 12:39:49 -0500

    success for stg_shopify_customers for snowflake and redshift

---
+++ macros/stitch/base/stitch_shopify_customers.sql
@@ -0,0 +1,63 @@
+{% macro stitch_shopify_customers() %}
+
+    {{ adapter_macro('shopify.stitch_shopify_customers') }}
+
+{% endmacro %}
+
+{% macro default__stitch_shopify_customers() %}
+
+    select
+        
+        --ids
+        id as customer_id,
+        
+        --dimensions
+        nullif(lower(email), '') as email,
+        verified_email,
+        nullif(lower(first_name), '') as first_name,
+        nullif(lower(last_name), '') as last_name,
+        accepts_marketing,
+        state,
+        tags,
+        tax_exempt,
+        
+        default_address__id as default_address_id,
+        note,
+        
+        --dates
+        created_at,
+        updated_at
+        
+    from
+        {{ var('customers_table') }}
+
+{% endmacro %}
+
+{% macro snowflake__stitch_shopify_customers() %}
+
+    select
+        
+        --ids
+        id as customer_id,
+        
+        --dimensions
+        nullif(lower(email), '') as email,
+        verified_email,
+        nullif(lower(first_name), '') as first_name,
+        nullif(lower(last_name), '') as last_name,
+        accepts_marketing,
+        state,
+        tags,
+        tax_exempt,
+        
+        default_address:id::number(38,6) as default_address_id,
+        note,
+        
+        --dates
+        created_at,
+        updated_at
+        
+    from
+        {{ var('customers_table') }}
+
+{% endmacro %}
--- models/base/stg_shopify_customers.sql
+++ models/base/stg_shopify_customers.sql
@@ -1,16 +1,4 @@
-select
-  id,
-  first_name,
-  last_name,
-  email,
-  verified_email,
-  default_address__id as default_address_id,
-  accepts_marketing,
-  note,
-  state,
-  tags,
-  tax_exempt,
-  created_at,
-  updated_at
-from
-  {{ var('customers_table') }}
+--see shopify docs here:
+--https://help.shopify.com/en/api/reference/customers/customer
+
+{{ stitch_shopify_customers() }}
