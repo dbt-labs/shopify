4a8d3fa0d435e22d69c4688eff5f0e82b8641fdf
Erin Ogilvy <erin.ogilvy@gmail.com>
2019-02-18 13:11:45 -0500

    success for stg_shopify_products for snowflake and redshift

---
+++ macros/stitch/base/stitch_shopify_products.sql
@@ -0,0 +1,56 @@
+{% macro stitch_shopify_products() %}
+
+    {{ adapter_macro('shopify.stitch_shopify_products') }}
+
+{% endmacro %}
+
+{% macro default__stitch_shopify_products() %}
+
+    select
+
+        id as product_id,
+        nullif(lower(product_type), '') as product_type,
+        nullif(lower(title), '') as title,
+        handle,
+        published_scope,
+        tags,
+        vendor,
+
+        body_html,
+        image__src as image_url,
+
+        --dates
+        published_at,
+        created_at,
+        updated_at
+
+    from
+      {{ var('products_table') }}
+
+{% endmacro %}
+
+
+{% macro snowflake__stitch_shopify_products() %}
+
+    select
+
+        id as product_id,
+        nullif(lower(product_type), '') as product_type,
+        nullif(lower(title), '') as title,
+        handle,
+        published_scope,
+        tags,
+        vendor,
+
+        body_html,
+        image:src::string as image_url,
+
+        --dates
+        published_at,
+        created_at,
+        updated_at
+
+    from
+      {{ var('products_table') }}
+
+{% endmacro %}
--- models/base/stg_shopify_products.sql
+++ models/base/stg_shopify_products.sql
@@ -1,14 +1,2 @@
-select
 
-  id,
-  title,
-  product_type,
-  vendor,
-  tags,
-  handle,
-  published_at,
-  created_at,
-  updated_at
-
-from
-  {{ var('products_table') }}
+{{ stitch_shopify_products() }}
